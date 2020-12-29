f5385085e0c57e3a41511768ab477eb550b35b38
turdakovan <random_imba@mail.ru>
2020-12-29 21:19:15 +0300

    Add number of words

--- training/views.py
+++ training/views.py
@@ -1,6 +1,10 @@
 
 from django.shortcuts import render
 
+
+def count_of_word(text):
+    return len(text.split())
+
 def about(request):
     return render(request, 'This is about page')
 
@@ -10,4 +14,10 @@ def home(request):
 def reverse(request):
     user_text = request.GET['usertext']
     reversed_text = user_text[::-1]
-    return render(request, 'reverse.html', {'usertext': user_text, 'reversedtext': reversed_text})
+    num_of_word = count_of_word(user_text)
+
+    data = {'usertext': user_text,
+            'reversedtext': reversed_text,
+            'countofword': num_of_word
+            }
+    return render(request, 'reverse.html', data)
