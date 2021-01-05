62033d292711c6aa4c9e59a1773c377f35ed7e3f
turdakovan <random_imba@mail.ru>
2021-01-05 17:42:15 +0300

    Count of words

--- training/views.py
+++ training/views.py
@@ -1,10 +1,6 @@
 
 from django.shortcuts import render
 
-
-def count_of_word(text):
-    return len(text.split())
-
 def about(request):
     return render(request, 'This is about page')
 
@@ -14,10 +10,10 @@ def home(request):
 def reverse(request):
     user_text = request.GET['usertext']
     reversed_text = user_text[::-1]
-    num_of_word = count_of_word(user_text)
+    number_of_word = len(user_text.split())
 
     data = {'usertext': user_text,
             'reversedtext': reversed_text,
-            'countofword': num_of_word
+            'count_of_word': number_of_word
             }
     return render(request, 'reverse.html', data)
